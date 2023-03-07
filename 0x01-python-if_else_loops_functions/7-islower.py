47096084c0f2f85d402afe51e8c652d7eb84f538
Fatimah1403 <fattyhassan14@gmail.com>
2023-03-07 22:59:03 +0000

    Task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    for x in c:
-        if ord(c) >= 97 and ord(c) <= 122:
-            return True
-        else:
-            False
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        False
 
