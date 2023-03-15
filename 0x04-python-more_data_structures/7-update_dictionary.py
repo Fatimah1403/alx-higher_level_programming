7f2cdfb7d45b7ca79a40416ac361c18d6da2997f
Fatimah1403 <fattyhassan14@gmail.com>
2023-03-15 23:02:22 +0000

    Task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dict = []
-    for value in a_dictionary:
-        if not math.isnan(value):
-            new_dict.append(value)
+    a_dictionary[key] = value
+    return a_dictionary
