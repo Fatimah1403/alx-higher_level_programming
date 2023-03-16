e008b202b38994ba5c5f7535e9db8d29f57ebd0b
Fatimah1403 <fattyhassan14@gmail.com>
2023-03-16 02:06:51 +0000

    Task 10 update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
+    if a_dictionary is None:
         return None
     max_score = max(a_dictionary.values(), default=None)
     for key, value in a_dictionary.items():
