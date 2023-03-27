a3751e24f82ea60b59f8d005af5172fd25b6d3e8
Fatimah 1403 <fattyhassan14@gmail.com>
2023-03-27 15:13:38 +0000

    Task 4 fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 
 
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
+      new_list = []
     for i in range(0, list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
@@ -17,4 +17,4 @@ def list_division(my_list_1, my_list_2, list_length):
             div = 0
         finally:
             new_list.append(div)
-        return (new_list)
+    return (new_list)
