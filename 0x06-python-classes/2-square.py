09d37233e465ed79165c522ca6ea23be80bde008
Fatimah 1403 <fattyhassan14@gmail.com>
2023-03-29 16:24:58 +0000

    Task 2 update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,7 +13,7 @@ class Square:
             TypeError: If `size` type is not `int`.
             ValueError: If `size` is less than `0`.
         """
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
