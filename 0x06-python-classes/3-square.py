38f1bd40e593bec4bdee597417241846b4d53dd1
Fatimah 1403 <fattyhassan14@gmail.com>
2023-03-29 16:48:26 +0000

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-     """A class that represents a square"""
+    """A class that represents a square"""
 
     def __init__(self, size=0):
         """Initializing this square class
