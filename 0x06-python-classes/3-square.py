cd8579f5813b76ebe1f585c91ce048eb6dc71080
Fatimah 1403 <fattyhassan14@gmail.com>
2023-03-29 04:07:14 +0000

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=o):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return (size.__size ** 2)
