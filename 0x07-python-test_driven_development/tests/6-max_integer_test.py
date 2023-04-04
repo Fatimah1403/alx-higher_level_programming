c5896c98ea8148493536a6ea18aaaa8d9af0c462
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-04-04 11:55:55 -0400

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -15,17 +15,17 @@ class TestMaxInteger(unittest.TestCase):
     def test_single_element(self):
         """ Test for a single element """
         element = [5]
-        self.asserEqual(max_integer(element), 5)
+        self.assertEqual(max_integer(element), 5)
 
     def test_ordered_list(self):
         """ Test for an ordered list """
         ordered_list = [0, 1, 2, 3, 4, 5]
-        self.asserteEqual(max_integer(ordered_list), 5)
+        self.assertEqual(max_integer(ordered_list), 5)
 
     def test_unordered_list(self):
         """ Test for an unordered list """
         unordered_list = [0, 4, 2, 7, 9, 5]
-        self.asserteEqual(max_integer(unordered_list), 9)
+        self.assertEqual(max_integer(unordered_list), 9)
 
     def test_floats(self):
         """ Test for float lists """
@@ -35,7 +35,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_floats_and_ints(self):
         """ Test for floats and int of a list """
         float_int = [0.5, 4, 2.0, 7, 10, 9.0, 15.2]
-        self.asserteEqual(max_integer(float_int), 10)
+        self.assertEqual(max_integer(float_int), 10)
 
     def test_empty_string(self):
         """Test an empty string."""
@@ -49,7 +49,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_array_at_mid(self):
         """Test for array that has max at the middle."""
         arr_mid = [4, 5, 7, 3, 2]
-        self.assertEqual(max_integer(arr_mid), 8)
+        self.assertEqual(max_integer(arr_mid), 7)
 
     def test_array_at_end(self):
         """Test for array that has max at the end."""
