4fcb27c2ccecd31171280d66657189630840fa75
Fatimah 1403 <fattyhassan14@gmail.com>
2023-04-04 03:44:14 +0000

    Task 4 new

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Defines a text-indentation function."""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    word = 0
+
+    while word < len(text) and text(word) == " ":
+        word = word + 1
+
+    while word < len(text):
+        print(text[word], end="")
+        if text[word] == "\n" or text[word] in ".?:":
+            if text[word] in ".?:":
+                print("\n")
+            word += 1
+            while word < len(text) and text[word] == " ":
+                word += 1
+            continue
+        word += 1
