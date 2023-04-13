4d6ad8ea39c363c82cb7ab03a53a6424d7c4d399
Fatimah1403 <fattyhassan14@gmail.com>
2023-04-13 16:55:17 +0000

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines a file-writing function."""
 
-def read_file(filename="", text=""):
+def write_file(filename="", text=""):
     """Write a string to a UTF8 text file.
     Args:
         filename (str): The name of the file to write.
@@ -10,4 +10,4 @@ def read_file(filename="", text=""):
         The number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+        return (f.write(text))
