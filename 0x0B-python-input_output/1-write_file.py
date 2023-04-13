6d20594c8633b3e1f83e1012e08955738f3d74a9
Fatimah1403 <fattyhassan14@gmail.com>
2023-04-13 16:52:31 +0000

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def read_file(filename="", text=""):
     """Prints the contents of a UTF8 text file"""
     with open(filename, "w", encoding="utf-8") as f:
-        print(f.write(text), end="")
+        return(f.write(text))
