f5e5fa1fd3ec4a999cdcdde60df27b15cb685d09
Fatimah1403 <fattyhassan14@gmail.com>
2023-04-13 16:41:44 +0000

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Reading from a text file using (UTF8)"""
+"""This module defines a text file-reading function"""
 
 
 def read_file(filename=""):
-    """ Print the content of the utf-8 file """
-    with open('filename', encoding='utf-8') as f:
+    """Prints the contents of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
