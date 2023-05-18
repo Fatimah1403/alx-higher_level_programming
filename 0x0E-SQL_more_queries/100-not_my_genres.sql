ce21dc14260653c7fd8877011ee6727cb4c688e1
Fatimah1403 <fattyhassan14@gmail.com>
2023-05-18 20:40:51 +0000

    Task 17

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -14,5 +14,5 @@ INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-Where tv_shows.tittle = "Dexter")
+Where tv_shows.title = "Dexter")
 ORDER BY tv_genres.name;
