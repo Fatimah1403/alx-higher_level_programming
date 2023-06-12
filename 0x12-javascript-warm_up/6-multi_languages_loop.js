2c9d9866b47a41f3e62a6e22a4c92e895dfa8bc8
Fatimah1403 <fattyhassan14@gmail.com>
2023-06-12 22:00:20 +0000

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const myVars = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < myVars.length; i++) {
-  console.log(myVars[i]);
+
+for (let i = 0; i < myvars.length; ++i) {
+  console.log(languages[i]);
 }
 
