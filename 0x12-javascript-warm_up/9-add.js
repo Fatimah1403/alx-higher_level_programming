f4d275c83fb712ca6c762997b52bbb14c2ccb50f
Fatimah1403 <fattyhassan14@gmail.com>
2023-06-13 14:42:30 +0000

    Task 9 update

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
+function add (a, b) {
+  return a + b;
+}
+
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 
-function add (a, b) {
-	return (a + b);
-}
-console.log(add((a), (b)));
+console.log(add(a, b));
