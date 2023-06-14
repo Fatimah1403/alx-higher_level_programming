880ad3433bcbfed9dfd3eed38bb5b49c6cabca07
Fatimah1403 <fattyhassan14@gmail.com>
2023-06-14 00:26:11 +0000

    Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-// Prints a Rectangle with the parameters passed.
+// Prints a Rectangle with the parameters passed
 
 module.exports = class Rectangle {
   constructor (width, height) {
@@ -9,15 +9,15 @@ module.exports = class Rectangle {
       this.height = height;
     }
   }
-  
+
   print () {
-    for (let i = 0; i < this.height; i++) {
+    for (let i = 0; i < this.height; ++i) {
       let j = 0;
 
-      for (; j < this.width; j++) {
+      for (; j < this.width; ++j) {
         process.stdout.write('X');
-      
       }
+
       if (j === this.width) {
         console.log('');
       }
