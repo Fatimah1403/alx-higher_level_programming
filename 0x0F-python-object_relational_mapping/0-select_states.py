613ef3455152417286b2b302948713a80065225f
Fatimah1403 <fattyhassan14@gmail.com>
2023-06-19 02:58:46 +0000

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" Connect Database and query """
+"""Conect DataBase and query"""
 
+import MySQLdb
 import sys
-import MYSQLdb
 
-# db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
-
-if __name__ == '__main__':
-    db = MYSQLdb.connect(
-            host="localhost", port=3306, user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3])
-
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[2],
+                         passwd=sys.argv[1],
+                         db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
 
-    row_selected = cur.fetchall()
-
-    for row in row_selected:
+    # Execute the query
+    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC;")
+    for row in cur.fetchall():
         print(row)
+
+    cur.close()
+    db.close()
