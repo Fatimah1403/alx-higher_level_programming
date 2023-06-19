db75e1ebe9a3ea8290f8b192cc7a334207f04359
Fatimah1403 <fattyhassan14@gmail.com>
2023-06-19 13:02:40 +0000

    Task 5 updates

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,41 +1,41 @@
 #!/usr/bin/python3
-
 """
-A script that takes in the name of a state as an argument
-and lists all cities of that state, using the database hbtn_0e_4_usa
-
+This script  takes in the name of a state
+as an argument and lists all cities of that
+state, using the database `hbtn_0e_4_usa`.
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[2],
-                         passwd=sys.argv[1],
-                         db=sys.argv[3])
-    cur = db.cursor()
+if __name__ == '__main__':
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
-    # Execute the query
-    cur.execute("""
-        SELECT
-            cities.id, cities.name
-        FROM
-            cities
-        JOIN
-            states
-        ON
-            cities.state_id = states.id
-        WHERE
-            states.name LIKE BINARY %(state_name)s
-        ORDER BY
-            cities.id ASC
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+            ORDER BY
+                cities.id ASC
         """, {
             'state_name': argv[4]
         })
 
-    row_selected = cur.fetchall()
+        rows = cur.fetchall()
 
-    if row_selected is not None:
-        print(", ".join([row[1] for row in row_selected]))
+    if rows is not None:
+        print(", ".join([row[1] for row in rows]))
