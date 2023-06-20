a45f346465e4cc050c1b108b87e07cc43e7d6031
Fatimah1403 <fattyhassan14@gmail.com>
2023-06-20 14:01:04 +0000

    Task 15

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
+"""
+
+from sys import argv
+from relationship_city import City
+from relationship_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
+    Base.metadata.creat_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    new_state_cal = State(name='California')
+    session.add(new_state_cal)
+    session.commit()
+
+    new_city_san = City(name='San Francisco')
+    session.add(new_city_san)
+    session.commit()
+
+    session.close()
