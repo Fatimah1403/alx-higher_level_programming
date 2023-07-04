d82710309a2b12b964710ea61c17c362a402fdc3
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-04 17:49:01 +0000

    Task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Write a Python script that takes
+2 arguments in order to solve this challenge.
+
+The first argument will be the repository name
+The second argument will be the owner name
+"""
+import sys
+import urllib.requests
+
+if __name__ == "__main__":
+    rep_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = "https://developer.github.com/v3/repos/commits/".format(
+            owner_name, rep_name)
+    r = requests.get(url)
+    commits = r.json
+
+    try:
+        for x in range(10):
+            print("{}: {}".format(
+                commits[x].get("sha")
+                commits[x].get("commit").get("Author").get("name")))
+    except ValueError:
+        print("Not a valid JSON")
