0d2e3b3f2e74bb8b89a2df47e132bcd331924ac3
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-04 15:24:32 +0000

    Task 4 update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Use the package request"""
-if __name__ == "__main__":
-    import requests
+import requests
 
-    r = requests.get('https://intranet.hbtn.io/status')
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
