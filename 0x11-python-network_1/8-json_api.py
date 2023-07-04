205943f13f7b80b8222c71ef2442fd526863359e
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-04 15:51:07 +0000

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a letter and
+sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+The letter must be sent in the variable q
+- If no argument is given, set q=""
+- If the response body is properly JSON formatted
+and not empty, display the id and name like this: [<id>] <name>
+Otherwise:
+- Display Not a valid JSON if the JSON is invalid
+- Display No result if the JSON is empty
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        dic = {'q': ""}
+    else:
+        dic = {'q': sys.argv[1]}
+
+    try:
+        r = requests.post('http://0.0.0.0:5000/search_user', data=dic)
+        data = r.json()
+
+        if data:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
