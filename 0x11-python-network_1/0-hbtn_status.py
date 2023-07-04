69319454384461230d9024523592c339ecc0b3b1
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-04 14:41:38 +0000

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,14 +2,14 @@
 """
 A Python script that fetches https://alx-intranet.hbtn.io/status
 """
-import urlliib.request
+import urllib.request
+
 
 if __name__ == "__main__":
-    request = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
-        ty = response.read()
-        print("ty response")
-        print("\t- type: {}".format(type(ty)))
-        print("\t- content: {}".format(ty))
-        print("\t- utf8 content: {}".format(ty.decode("utf-8")))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
