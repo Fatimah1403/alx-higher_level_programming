70608399c0c94f1de8d812e60b258e55d8740e65
Fatimah <fattyhassan14@gmail.com>
2023-08-02 20:04:26 -0400

    Task 4

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,5 +1,11 @@
-$(document).ready(function () {
-    $("DIV#toggle_header").click(function () {
-        $("header").toggleClass("red green");
+$(document).ready(function() {
+    $('div#toggle_header').click(function() {
+      var header = $('header');
+      
+      if (header.hasClass('red')) {
+        header.removeClass('red').addClass('green');
+      } else {
+        header.removeClass('green').addClass('red');
+      }
     });
-});
+  });
